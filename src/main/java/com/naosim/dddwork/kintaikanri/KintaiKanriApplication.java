be58614ee9d00f399ab813bc2656c715ebb3291d
tsutsuihirofumi <h-tsutsui@cnt.biglobe.co.jp>
2018-02-07 15:59:43 +0900

    spring-bootで起動できるように新規追加

---
+++ src/main/java/com/naosim/dddwork/kintaikanri/KintaiKanriApplication.java
@@ -0,0 +1,12 @@
+package com.naosim.dddwork.kintaikanri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaiKanriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaiKanriApplication.class, args);
+	}
+}
---
+++ src/main/java/com/naosim/dddwork/kintaikanri/controller/SampleController.java
@@ -0,0 +1,7 @@
+package com.naosim.dddwork.kintaikanri.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+}
