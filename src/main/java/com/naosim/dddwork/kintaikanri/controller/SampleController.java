cc9db3cb2554a8d271235bd03c37e45f4e0078b3
tsutsuihirofumi <h-tsutsui@cnt.biglobe.co.jp>
2018-02-07 16:03:27 +0900

    サンプルURLの追加

--- src/main/java/com/naosim/dddwork/kintaikanri/controller/SampleController.java
+++ src/main/java/com/naosim/dddwork/kintaikanri/controller/SampleController.java
@@ -1,7 +1,13 @@
 package com.naosim.dddwork.kintaikanri.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+
+    @RequestMapping("/sample")
+    public String sample() {
+        return "これはサンプルです。";
+    }
 }
