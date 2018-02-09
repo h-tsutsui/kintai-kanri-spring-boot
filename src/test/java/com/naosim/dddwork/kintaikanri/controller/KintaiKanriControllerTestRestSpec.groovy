f1e017cf60dcbd9338445b58c98f2ac2ec683e87
tsutsuihirofumi <h-tsutsui@cnt.biglobe.co.jp>
2018-02-09 17:45:30 +0900

    war作成対応

--- src/test/java/com/naosim/dddwork/kintaikanri/controller/KintaiKanriControllerTestRestSpec.groovy
+++ src/test/java/com/naosim/dddwork/kintaikanri/controller/KintaiKanriControllerTestRestSpec.groovy
@@ -6,12 +6,13 @@ import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.http.*
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import spock.lang.Specification
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class KintaiKanriController1Spec extends Specification {
+class KintaiKanriControllerTestRestSpec extends Specification {
 
     @Autowired
     TestRestTemplate testRestTemplate
@@ -26,12 +27,6 @@ class KintaiKanriController1Spec extends Specification {
         when:
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON)
-
-        RequestEntity<?> req = new RequestEntity<>(workTimeInputForm, headers, HttpMethod.POST, "/api/kintai/input")
-        HttpEntity<WorkTimeInputForm> entity = new HttpEntity<WorkTimeInputForm>(headers);
-
-        ResponseEntity<String> response = testRestTemplate.exchange(req, )
-        println("result >>>> " + response.getStatusCode())
 
         then:
         true
